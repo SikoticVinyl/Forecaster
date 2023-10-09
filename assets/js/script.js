d27b3f525980b9de40cff7ddbb8464e8ca991ec5
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2023-10-08 23:03:30 -0600

    built out fetch request more.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,33 @@
 // API Key? dd470cbf4f5da7a6f9f1f03c52320e07
-// URL for Fetch: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+//URL for Fetch: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 const searchBtn = document.querySelector(".searchBtn");
 const cityList = document.querySelector(".savedCity");
 const fiveDay = document.querySelector(".fiveDay");
 const nowWeather = document.querySelector(".searchedCity");
 const userInput = document.querySelector(".cityS");
 
-const apiKey="";
-const url="";
+const apiKey="dd470cbf4f5da7a6f9f1f03c52320e07";
+const url=`api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`;
 const gUrl="";
+
+let lat=""
+let lon=""
 
 function getWeather () {
 
-    fetch(url, {
-        method: 'GET'
-    })
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Fetch error: ', error);
+        });
 }
 
+//searchBtn.addEventListener('click', function
