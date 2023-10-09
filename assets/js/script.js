d2329af12e3b7e933391d8d3bb967d47539653a2
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2023-10-08 23:40:17 -0600

    first fetch request working.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,27 +7,57 @@ const nowWeather = document.querySelector(".searchedCity");
 const userInput = document.querySelector(".cityS");
 
 const apiKey="dd470cbf4f5da7a6f9f1f03c52320e07";
-const url=`api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`;
-const gUrl="";
+//const url=`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-let lat=""
-let lon=""
+let cityName=""
 
-function getWeather () {
+function searchCity(){
 
-    fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok.');
-            }
+    cityName=userInput.value; //sets the user input to the city name
+
+    const gUrl=`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`; //API URL
+    console.log("URL: ", gUrl);
+
+    fetch(gUrl) //The fetch request
+
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok.')
+        }
             return response.json();
-        })
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('Fetch error: ', error);
-        });
-}
+    })
+    .then(data => {
+        console.log('API Response Data:', data);
+    })
+    .catch(error => {
+        console.error('Fetch error: ', error);
+    });
+};
 
-//searchBtn.addEventListener('click', function
+
+let lat="" //2 in array?
+let lon="" //3 ' '
+
+//function getWeather() {
+//
+  //  fetch(url)
+//
+  //  .then(response => {
+    //    if (!response.ok) {
+      //      throw new Error('Network response was not ok.');
+        //}
+        //    return response.json();
+    //})
+    //.then(data => {
+    //    console.log(data);
+    //})
+    //.catch(error => {
+    //    console.error('Fetch error: ', error);
+    //});
+//}
+
+searchBtn.addEventListener('click', function (){
+    console.log("Button clicked");
+    console.log("Input: ", cityName);
+    searchCity()
+});
